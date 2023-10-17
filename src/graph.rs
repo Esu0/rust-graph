06897acf819f287970334a63bc46239a01c498d7
Esu <87416336+Esu0@users.noreply.github.com>
2023-10-17 15:33:12 +0000

    抽象化したグラフトレイトを定義

--- src/graph.rs
+++ src/graph.rs
@@ -1,1 +1,30 @@
-pub trait Graph {}
+#![allow(unused)]
+
+use std::collections::BinaryHeap;
+pub trait Heap: Default {
+    type Elem;
+
+    fn push(&mut self, elem: Self::Elem);
+    fn pop(&mut self) -> Option<Self::Elem>;
+}
+
+pub trait Graph<V: Copy, H: Heap<Elem = V>> {
+    fn adj_v(&self, vertex: V, heap: &mut H);
+    fn dfs(&self, root: V) -> IterDfs<'_, V, H, Self> {
+        IterDfs {graph: self, current: root, heap: H::default()}
+    }
+}
+
+pub struct IterDfs<'a, V, H, G: ?Sized> {
+    graph: &'a G,
+    current: V,
+    heap: H,
+}
+
+impl<'a, V: Copy, H: Heap<Elem = V>, G: ?Sized + Graph<V, H>> Iterator for IterDfs<'a, V, H, G> {
+    type Item = V;
+    fn next(&mut self) -> Option<Self::Item> {
+        // implements
+        None
+    }
+}
